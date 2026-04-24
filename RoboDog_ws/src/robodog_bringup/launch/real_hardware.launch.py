8da13940d9c1abbf74c1c0d36a3d42742801ddf9
Ahmed Hamouda <142184407+Ahmed-Hammoda@users.noreply.github.com>
2026-04-24 17:18:44 +0100

    Delete RoboDog_ws directory

--- RoboDog_ws/src/robodog_bringup/launch/real_hardware.launch.py
+++
@@ -1,7 +0,0 @@
-from launch import LaunchDescription
-
-from robodog_bringup.joystick_launch_common import build_joystick_stack
-
-
-def generate_launch_description() -> LaunchDescription:
-    return LaunchDescription(build_joystick_stack('laptop_usb_joystick.yaml'))
